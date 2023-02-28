5d4ece382e504350641beec3a9d5a890379b4509
udayfakie <92738263+udayfakie@users.noreply.github.com>
2023-02-28 23:49:12 +0200

    build

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,36 @@
-import express from 'express';
-import data from './data.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import seedRouter from './routes/seedRouter.js';
-import productRouter from './routes/productRouter.js'
-import userRouter from './routes/userRoutes.js';
+import express from "express";
+import data from "./data.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import seedRouter from "./routes/seedRouter.js";
+import productRouter from "./routes/productRouter.js";
+import userRouter from "./routes/userRoutes.js";
 
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGODB_URI)
-.then(() => {
-  console.log('connected to db');
-})
-.catch((err) => {
-  console.log(err.message);
-});
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/seed' , seedRouter);
-app.use('/api/products' , productRouter);
-app.use('/api/users' , userRouter);
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
-
-app.use((err,req,res,next) =>{
-  res.status(500).send({message: err.message})
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`server at http://localhost:${port}`);
+  console.log(`server at http://localhost:${port}`);
 });
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,10 @@
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import Rating from './Rating';
-import axios from 'axios';
-import { useContext } from 'react';
-import { Store } from '../Store';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import axios from "axios";
+import { useContext } from "react";
+import { Store } from "../Store";
 
 function Product(props) {
   const { product } = props;
@@ -19,11 +19,11 @@ function Product(props) {
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${item._id}`);
     if (data.countInStock < quantity) {
-      window.alert('Sorry. Product is out of stock');
+      window.alert("Sorry. Product is out of stock");
       return;
     }
     ctxDispatch({
-      type: 'CART_ADD_ITEM',
+      type: "CART_ADD_ITEM",
       payload: { ...item, quantity },
     });
   };
@@ -38,14 +38,14 @@ function Product(props) {
           <Card.Title>{product.name}</Card.Title>
         </Link>
         <Rating rating={product.rating} numReviews={product.numReviews} />
-        <Card.Text>${product.price}</Card.Text>   
+        <Card.Text>${product.price}</Card.Text>
         {product.countInStock === 0 ? (
           <Button variant="light" disabled>
             Out of stock
           </Button>
         ) : (
           <Button onClick={() => addToCartHandler(product)}>Add to cart</Button>
-          )}
+        )}
       </Card.Body>
     </Card>
   );
