f86d6ae58f297a0bdc7a24aeb29d47876d6e6f0a
udayfakie <92738263+udayfakie@users.noreply.github.com>
2023-03-22 21:09:52 +0200

    p/json modif

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import seedRouter from "./routes/seedRouter.js";
 import productRouter from "./routes/productRouter.js";
 import userRouter from "./routes/userRoutes.js";
+import OrderRouter from './routes/OrderRouter.js';
 
 dotenv.config();
 
@@ -22,9 +23,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/api/keys/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
 app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
+app.use('/api/orders', OrderRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
